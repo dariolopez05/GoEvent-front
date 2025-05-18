7f079c75866064aac1d86c821b6d04574b74dc52
dariolpz05 <aodariolopez@gmail.com>
2025-05-18 19:20:32 +0200

    Arreglando servicio de favoritos

--- src/services/ticketmaster.service.ts
+++ src/services/ticketmaster.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Ticketmaster } from '../models/ticketmaster.interface';
 import { Router } from '@angular/router';
+import { timer } from 'rxjs';
+import { retryWhen, delayWhen, scan } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,17 +21,27 @@ export class TicketmasterService {
 
   public getResponseId(id: string): Observable<Ticketmaster> {
   const url = `https://app.ticketmaster.com/discovery/v2/events?countryCode=ES&apikey=${this.apiKey}&id=${id}`;
+  
   return this.http.get<Ticketmaster>(url).pipe(
+    retryWhen(errors =>
+      errors.pipe(
+        scan((retryCount, error) => {
+          if (retryCount >= 3 || error.status !== 429) throw error;
+          return retryCount + 1;
+        }, 0),
+        delayWhen(() => timer(1000))
+      )
+    ),
     catchError(error => {
       console.log('Error en getResponseId:', error);
       if (error.status === 429) {
-        window.alert('Algo ha fallado. Por favor espera unos segundos e inténtalo de nuevo.');
+        window.alert('Demasiadas peticiones. Espera unos segundos e inténtalo de nuevo.');
         this.router.navigate(['/home']);
       }
       return throwError(() => error);
     })
   );
-}
+  }
 
   public getResponsePagination(page: number, city: string, query:string): Observable<Ticketmaster> {
     const url = `https://app.ticketmaster.com/discovery/v2/events?countryCode=ES&apikey=${this.apiKey}&page=${page}&city=${city}&keyword=${query}`;
