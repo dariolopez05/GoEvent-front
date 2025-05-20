395e3497b56487e6345fbf3c5835acb5ee9622dd
dariolpz05 <aodariolopez@gmail.com>
2025-05-20 08:51:18 +0200

    Actualizando servicio para que la funcion getResponseId trabaje con el back, asi solucionamos problema de cors

--- src/services/ticketmaster.service.ts
+++ src/services/ticketmaster.service.ts
@@ -5,12 +5,14 @@ import { Ticketmaster } from '../models/ticketmaster.interface';
 import { Router } from '@angular/router';
 import { timer } from 'rxjs';
 import { retryWhen, delayWhen, scan } from 'rxjs/operators';
+import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketmasterService {
   public apiKey: string = 'tZ08mVmIlAuo2fAnahB2eYN5ThiLLaQY';
+  private apiTicketMaster = `${environment.apiUrl}/api/ticketmaster`;
 
   constructor(public http: HttpClient, private router: Router) { }
 
@@ -20,25 +22,24 @@ export class TicketmasterService {
   }
 
  public getResponseId(id: string): Observable<Ticketmaster> {
-  const url = `https://app.ticketmaster.com/discovery/v2/events?countryCode=ES&apikey=${this.apiKey}&id=${id}`;
-  
-  return this.http.get<Ticketmaster>(url).pipe(
-    catchError(error => {
-      console.error('Error en getResponseId:', error);
+    return this.http.get<Ticketmaster>(`${this.apiTicketMaster}?id=${id}`).pipe(
+      catchError(error => {
+        console.error('Error en getResponseId:', error);
 
-      if (error.status === 429) {
-        window.alert('Demasiadas peticiones. Por favor espera unos segundos e inténtalo de nuevo.');
-        this.router.navigate(['/home']);
-      } else if (error.status === 0) {
-        window.alert('Error de conexión o CORS. Intenta recargar la página.');
-      } else {
-        window.alert('Ha ocurrido un error inesperado.');
-      }
+        if (error.status === 429) {
+          window.alert('Demasiadas peticiones. Por favor espera unos segundos e inténtalo de nuevo.');
+          this.router.navigate(['/home']);
+        } else if (error.status === 0) {
+          window.alert('Error de conexión con el backend. Intenta recargar la página.');
+        } else {
+          window.alert('Ha ocurrido un error inesperado.');
+        }
 
-      return EMPTY; 
-    })
-  );
-}
+        return EMPTY; 
+      })
+    );
+  }
+
 
 
   public getResponsePagination(page: number, city: string, query:string): Observable<Ticketmaster> {
